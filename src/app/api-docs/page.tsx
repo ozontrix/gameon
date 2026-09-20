93298a520527c7a48d499240f84a99bb58671399
ozontrix <ozontrix@gmail.com>
2026-09-20 13:04:11 +0530

    new changes in api

--- src/app/api-docs/page.tsx
+++ src/app/api-docs/page.tsx
@@ -2,7 +2,7 @@
 
 import { ApiReferenceReact } from '@scalar/api-reference-react';
 import '@scalar/api-reference-react/style.css';
-
+ 
 export default function ApiDocsPage() {
   return (
     <div style={{ height: '100vh', width: '100vw' }}>
